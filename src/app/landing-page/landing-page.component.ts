8a0ab5aa67da0b07f7dcad76b5753aee58aa85ef
DaMexicanJustice <mrrichtofen@gmail.com>
2023-03-23 20:04:28 +0100

    Worked on that damn down caret

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import { faTwitch, faYoutube, faPatreon } from '@fortawesome/free-brands-svg-icons'
-import { trigger, state, style, animate, transition } from '@angular/animations'
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations'
 import { timer } from 'rxjs';
 
 @Component({
@@ -11,12 +11,27 @@ import { timer } from 'rxjs';
   animations: [
     trigger('slideIn', [
       state('hidden', style({
-        //transform: 'translateX(-1000%)'
+        transform: 'translateX(-1000%)'
       })),
       state('visible', style({
-        //transform: 'translateX(-0%)'
+        transform: 'translateX(-0%)'
       })),
       transition('hidden => visible', animate('1000ms cubic-bezier(0.175, 0.885, 0.32, 1.275)')),
+      transition('visible => hidden', animate('1000ms cubic-bezier(0.175, 0.885, 0.32, 1.275)')),
+    ]),
+    trigger('sinusoidal', [
+      transition('* => *', [
+        animate('2s',
+          keyframes([
+            style({transform: 'translateY(0)', offset: 0}),
+            style({transform: 'translateY(-10px)', offset: 0.2}),
+            style({transform: 'translateY(0)', offset: 0.4}),
+            style({transform: 'translateY(10px)', offset: 0.6}),
+            style({transform: 'translateY(0)', offset: 0.8}),
+            style({transform: 'translateY(-10px)', offset: 1.0}),
+          ])
+        )
+      ])
     ])
   ]
 })
